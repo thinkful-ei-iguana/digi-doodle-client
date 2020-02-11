8f89539ad6678bb74ea1b3ee757372443c2c1dfc
Jon <jonathanjackson424@gmail.com>
2020-02-11 17:33:51 -0500

    setting up more routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import './App.css';
-// import DrawingPage from './DrawingPage/DrawingPage'
+import GuessingPage from './GuessingPage/GuessingPage';
+import DrawingPage from './DrawingPage/DrawingPage';
+import GameLobbyPage from './GameLobbyPage/GameLobbyPage';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={LandingPage} />
+      <Route exact path='/guess' component={GuessingPage} />
+      <Route exact path='/draw' component={DrawingPage} />
+      <Route exact path='/lobby' component={GameLobbyPage} />
     </div>
   );
 }
