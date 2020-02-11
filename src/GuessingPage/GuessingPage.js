6c44335a6118736979f8be81b92a1c50611a14f9
Sophia Koeut <koeutsophia@gmail.com>
2020-02-11 11:27:13 -0800

    basic wireframe guessing page and drawing page down

--- src/GuessingPage/GuessingPage.js
+++ src/GuessingPage/GuessingPage.js
@@ -11,12 +11,19 @@ export default class GuessingPage extends Component {
     // });
 
 
+    //event handler for submit button to validate answer
+
     render() {
         return (
             <div>
-                <h1>You are Drawing!</h1>
-                <h3>Draw a fish </h3>
+                <h1>What are they drawing?</h1>
                 <p>this is canvas</p>
+                <div>player 1</div>
+                <div>player 2</div>
+                <div>player 3</div>
+                <div>player 4</div>
+                <input type="text" placeholder="type your guess here"></input>
+                <button>Go!</button>
                 {/* <canvas id="cfd" className="canvas-container"></canvas> */}
             </div>
         )
