3b8e32489c6822155079279061965135b26d7ac7
Jon <jonathanjackson424@gmail.com>
2020-02-11 13:15:31 -0500

    added logo and bare bones structure to landing page

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './LandingPage.css';
+import Logo from '../../Pictures/digidoodle-logo.png';
+
+//title display
+//logo
+// description
+//start button
+
+class LandingPage extends React.Component {
+  state = {}
+  render() {
+    return (
+      <div className="landing-page">
+        <div className="logo-container">
+          <img className="logo" src={Logo} alt="logo" />
+        </div>
+        <div className="about-container">
+          <p className="about-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+
+        </div>
+
+        <button className="start-button">Draw</button>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
