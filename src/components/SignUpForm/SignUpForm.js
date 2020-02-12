a0f3b898c8ac8c798cf945ff73875ec5dd42f3b1
Jon <jonathanjackson424@gmail.com>
2020-02-12 18:49:28 -0500

    made the Enter button work for the submit

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -21,24 +21,17 @@ class SignUpForm extends React.Component {
     });
   }
 
-
   handleSubmit = (event) => {
     let username = this.state.username;
     if (username.length < 4 || username.length > 10) {
+      event.preventDefault();
       return this.setState({
         error: {
           error: 'Please choose a username between 4 and 10 characters'
         }
       })
-      // return alert("Please choose a username between 4 and 10 characters.")
     }
     event.preventDefault();
-    this.setState({
-      error: {
-        error: ''
-      },
-    })
-
     DigiDoodleApiService.createUserName(username).catch(res => {
       this.setState({
         error: {
@@ -62,18 +55,14 @@ class SignUpForm extends React.Component {
       message = <h1>Hello, {username}!</h1>
     }
 
-    if (error) {
-      error = <h1>{this.state.error.error}</h1>
-    }
-
     return (
       <div className="sign-up-page">
         <div className="logo-container">
           <img className="logo" src={Logo} alt="logo" />
         </div>
 
-        <div className="sign-up-form">
-          <form>
+        <div className="sign-up-form-container" onSubmit={this.handleSubmit}>
+          <form className="sign-up-form">
             {errorMessage}
             {message}
             <p>Enter your username:</p>
@@ -82,10 +71,12 @@ class SignUpForm extends React.Component {
               onChange={(event) => this.handleChange(event)}
               required
             />
+            <br></br>
+            <button className="start-button" type="submit"  >Play Game!</button>
           </form>
         </div>
 
-        <button className="start-button" onClick={this.handleSubmit} >Play Game!</button>
+
       </div>
     );
   }
