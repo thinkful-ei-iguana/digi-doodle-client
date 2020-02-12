f49f35df8e08196a4c3a3d231a811e569f31e201
Sophia Koeut <koeutsophia@gmail.com>
2020-02-12 09:31:26 -0800

    added config/env files and apiservice

---
+++ services/digi-doodle-api-service.js
@@ -0,0 +1,17 @@
+import config from '../src/config'
+
+const DigiDoodleApiService = {
+    createUserName(username) {
+        return fetch(`${config.API_ENDPOINT}/player`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ username })
+        }).then(res =>
+            !res.ok ? res.json().then(event => Promise.reject(event)) : res.json()
+        );
+    }
+};
+
+export default DigiDoodleApiService
