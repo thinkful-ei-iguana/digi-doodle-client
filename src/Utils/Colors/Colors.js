7f121f845b62d7e8183f7be0649cb808eaba9bb1
austint33 <austintumlinson@gmail.com>
2020-02-12 17:15:35 -0600

    stuck

--- src/Utils/Colors/Colors.js
+++ src/Utils/Colors/Colors.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 class Colors extends React.Component {
-    render() {
+    render(props) {
         return (
             <div className="color-buttons">
-                <button onClick={this.props.changeColorBlack} type="button" className="black"></button>
-                <button onClick={this.props.changeColorBrown} type="button" className="brown"></button>
-                <button onClick={this.props.changeColorRed} type="button" className="red"></button>
-                <button onClick={this.props.changeColorYellow} type="button" className="yellow"></button>
-                <button onClick={this.props.changeColorOrange} type="button" className="orange"></button>
-                <button onClick={this.props.changeColorPurple} type="button" className="purple"></button>
-                <button onClick={this.props.changeColorBlue} type="button" className="blue"></button>
-                <button onClick={this.props.changeColorGreen} type="button" className="green"></button>
-                <button onClick={this.props.handleEraser} type="button" className="eraser"></button>
+                <button onClick={(props)=>this.props.changeColorBlack} type="button" className="black"></button>
+                <button onClick={(props)=>this.props.changeColorBrown} type="button" className="brown"></button>
+                <button onClick={(props)=>this.props.changeColorRed} type="button" className="red"></button>
+                <button onClick={(props)=>this.props.changeColorYellow} type="button" className="yellow"></button>
+                <button onClick={(props)=>this.props.changeColorOrange} type="button" className="orange"></button>
+                <button onClick={(props)=>this.props.changeColorPurple} type="button" className="purple"></button>
+                <button onClick={(props)=>this.props.changeColorBlue} type="button" className="blue"></button>
+                <button onClick={(props)=>this.props.changeColorGreen} type="button" className="green"></button>
+                <button onClick={(props)=>this.props.handleEraser} type="button" className="eraser"></button>
             </div>
         )
     }
