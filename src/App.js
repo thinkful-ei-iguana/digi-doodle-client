029fe63c7018a5068101533f5b8335457f92a54a
AustinT33 <austintumlinson@gmail.com>
2020-02-12 10:01:41 -0600

    added state and context for app to allow state to be passed anywhere we want

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
+import AppContext from './Context/AppContext'
 import './App.css';
 import GuessingPage from './GuessingPage/GuessingPage';
 import DrawingPage from './DrawingPage/DrawingPage';
 import GameLobbyPage from './GameLobbyPage/GameLobbyPage';
 
-function App() {
-  return (
-    <div className="App">
-      <Route exact path='/' component={LandingPage} />
-      <Route exact path='/guess' component={GuessingPage} />
-      <Route exact path='/draw' component={DrawingPage} />
-      <Route exact path='/lobby' component={GameLobbyPage} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+      <div className="App">
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/guess' component={GuessingPage} />
+        <Route exact path='/draw' component={DrawingPage} />
+        <Route exact path='/lobby' component={GameLobbyPage} />
+      </div>
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default App;
