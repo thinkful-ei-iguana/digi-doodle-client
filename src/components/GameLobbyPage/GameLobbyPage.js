547d7b6756a8f4e323506f084c8ca2d24ba180ab
Michael Weedman <michael.weedman@gmail.com>
2020-02-16 21:16:14 -0700

    adding socket io

--- src/components/GameLobbyPage/GameLobbyPage.js
+++ src/components/GameLobbyPage/GameLobbyPage.js
@@ -2,22 +2,37 @@ import React, { Component } from 'react'
 import DrawingPage from '../DrawingPage/DrawingPage'
 import DigiDoodleApiService from '../../services/digi-doodle-api-service';
 import ColorContext from '../../Context/ColorContext';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
+import socketIOClient from 'socket.io-client';
 import './GameLobbyPage.css'
 
 export default class GameLobbyPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            endpoint: ''            
+        }
     }
 
     static contextType = ColorContext
 
-    componentDidMount() {
+    async componentDidMount() {
         let cookie = Cookies.get();
         let data = JSON.parse(cookie['digi-doodle-user']);
         console.log('BANG', data);
 
+        // FOR SOCKET //
+       
+        await this.setState({
+            endpoint: `localhost:8000/api/game/${data.gameId}`
+        }) 
+        
+        const socket = socketIOClient(this.state.endpoint);
+
+        socket.on('guess submitted', (res) => {
+            console.log(res.json());
+        })
+        // END OF SOCKET SETUP //
         this.context.setGameId(data.gameId)
         this.context.setUserName(data.username)
         this.context.setUserId(data.userId)
@@ -33,6 +48,7 @@ export default class GameLobbyPage extends Component {
                 this.context.setPlayers(playersArray)
             })
     }
+
     render() {
 
         return (
