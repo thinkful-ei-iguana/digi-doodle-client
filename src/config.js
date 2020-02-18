9125eeb21143cd58e883e21b0260ab00818b2f6b
Michael Weedman <michael.weedman@gmail.com>
2020-02-18 15:38:32 -0700

    more shit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     API_ENDPOINT:
-        // process.env.REACT_APP_API_ENDPOINT || 
-        "http://localhost:8000/api",
+        process.env.REACT_APP_API_ENDPOINT || 
+        "https://digi-doodle-tester.herokuapp.com/api",
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
+    SOCKET_ENDPOINT: "https://digi-doodle-tester.herokuapp.com"
 };
