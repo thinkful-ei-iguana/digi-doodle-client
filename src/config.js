fb0dbe83a3970b7c3944c804890e69a8bb25e240
Sophia Koeut <koeutsophia@gmail.com>
2020-02-20 14:50:45 -0800

    changed config file for deploy

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 export default {
     API_ENDPOINT:
-        process.env.REACT_APP_API_ENDPOINT || 
-        // "https://digi-doodle-tester.herokuapp.com/api",
-        "http://localhost:8000/api",
+        process.env.REACT_APP_API_ENDPOINT ||
+        "https://digi-doodle.herokuapp.com/api",
+    //"http://localhost:8000/api",
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
-    SOCKET_ENDPOINT: "http://localhost:8000"
-    // "https://digi-doodle-tester.herokuapp.com"
-    
+    SOCKET_ENDPOINT: "https://digi-doodle.herokuapp.com"
+
 };
