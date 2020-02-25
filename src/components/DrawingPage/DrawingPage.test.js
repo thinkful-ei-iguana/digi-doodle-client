9343b9c87f1a35261aa2a8b99f1eae66bac2220c
Jon <jonathanjackson424@gmail.com>
2020-02-25 17:22:04 -0500

    got some (one) tests to work

--- src/components/DrawingPage/DrawingPage.test.js
+++ src/components/DrawingPage/DrawingPage.test.js
@@ -1,13 +1,27 @@
 import React from 'react'; 
 import DrawingPage from './DrawingPage'
+import { Canvas } from './DrawingPage'
 import ReactDOM from 'react-dom';
+import App from '../../App';
 import { BrowserRouter } from 'react-router-dom';
+import { shallow, Enzyme } from 'enzyme';
+import '../../setupTests'
+
+describe ('App component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+})
 
 
 describe ('DrawingPage component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><DrawingPage/></BrowserRouter>, div);
+    ReactDOM.render(<Canvas/>, div);
     ReactDOM.unmountComponentAtNode(div);
   })
 })
+
+
