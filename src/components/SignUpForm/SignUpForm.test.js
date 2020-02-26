a3e64dc8c47dfe6e99597143650df50c746645c2
Jon <jonathanjackson424@gmail.com>
2020-02-26 12:37:45 -0500

    added SignUpForm behavioral tests

--- src/components/SignUpForm/SignUpForm.test.js
+++ src/components/SignUpForm/SignUpForm.test.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import SignUpForm from './SignUpForm'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { mount } from 'enzyme'
+
+//Enzyme.configure({adapter: new Adapter()})
+
+//const [title, setTitle] = React.useState('')
 
 
 describe(`SignupForm component`, () => {
@@ -21,3 +28,14 @@ describe(`SignupForm component`, () => {
     })
 
 })
+
+it('renders text input with label', () => {
+    const wrapper = mount(<SignUpForm /> )
+    const label = wrapper.find('label')
+
+    const input = wrapper.find('input');
+    expect(input).toHaveLength(1);
+    expect(input.prop('type')).toEqual('text')
+})
+
+
