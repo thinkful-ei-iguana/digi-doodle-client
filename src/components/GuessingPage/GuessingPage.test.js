05e8121b2f2ff5cd30a6451cfb8529a93306654c
Jon <jonathanjackson424@gmail.com>
2020-02-26 12:45:42 -0500

    adding guessingpage tests

--- src/components/GuessingPage/GuessingPage.test.js
+++ src/components/GuessingPage/GuessingPage.test.js
@@ -19,7 +19,21 @@ describe(`GuessingPage component`, () => {
 
       ReactDOM.unmountComponentAtNode(div)
   })
+  it('renders GuessingPage', () => {
+    const form = renderer.create(<GuessingPage />).toJSON();
+    expect(form).toMatchSnapshot();
+})
 
+})
+
+it('renders text input with label', () => {
+const wrapper = mount(<GuessingPage /> )
+const label = wrapper.find('label')
+
+const input = wrapper.find('input');
+expect(input).toHaveLength(1);
+expect(label).toHaveLength(1);
+expect(input.prop('type')).toEqual('text')
 })
 
 // it('renders text input with label', () => {
