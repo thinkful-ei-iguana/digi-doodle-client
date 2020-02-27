88025e3e80f04e1669e0a76a585d90c14748e34c
Jon <jonathanjackson424@gmail.com>
2020-02-27 12:57:21 -0500

    changed Utils to match Sophia's stuff

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import GameLobbyPage from './components/GameLobbyPage/GameLobbyPage';
 import { ColorProvider } from './Context/ColorContext';
-import PrivateRoute from '../src/Utils/PrivateRoute'
-import PublicRoute from '../src/Utils/PublicOnlyRoute'
 import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 import './App.css';
 import SignUpForm from './components/SignUpForm/SignUpForm';
@@ -24,8 +22,8 @@ class App extends React.Component {
       <ColorProvider>
         <div className="App">
           <Switch>
-            <PublicRoute exact path='/' component={SignUpForm} />
-            <PrivateRoute path='/lobby' component={GameLobbyPage} />
+            <Route exact path='/' component={SignUpForm} />
+            <Route path='/lobby' component={GameLobbyPage} />
             <BrowserRouter>
             <Route path="*" component={NotFoundPage} />
             </BrowserRouter>
