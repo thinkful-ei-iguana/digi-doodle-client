16993d731f42d7755aca2180b5ecdcd82ab00fdb
Jon <jonathanjackson424@gmail.com>
2020-02-27 12:16:26 -0500

    notfoundpage final commit

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -4,9 +4,9 @@ import './NotFoundPage.css'
 
 
 const NotFound = () => (
-<div>
-<h1>Page Not Found...</h1>
-<center><Link className="return-home" style={{textDecoration: 'none'}} to="/">Return to Home Page</Link></center>
-</div>
+  <div>
+    <h1>Page Not Found...</h1>
+    <center><Link className="return-home" style={{ textDecoration: 'none' }} to="/">Return to Home Page</Link></center>
+  </div>
 );
 export default NotFound;
